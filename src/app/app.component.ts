be47a3a2f21ffe089602ba13bc5a7ee9488c0a28
Larry N <larryn000@gmail.com>
2025-09-08 17:31:06 -0400

    fix: version as of interview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'fico-tax-utility';
+
+
+  @Input() income: string = "";
+  @Input() state: string = "AZ";
+
+  taxPaid: number = 0;
+
+  stateTax = {
+    "AZ": 0.05,
+    "VA": 0.03
+  }
+
+  onSubmit() {
+    let stateTax = 0;
+    switch (this.state) {
+      case "AZ":
+        stateTax = 0.05;
+        break;
+      case "VA":
+        stateTax = 0.03;
+        break;
+    }
+    this.taxPaid = (Number(this.income)) * stateTax;
+  }
 }
